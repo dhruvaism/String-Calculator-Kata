66496297ae60871216d5cf1d06f2120226f4c8ab
k-kgs <b170090@nitsikkim.ac.in>
2020-10-24 22:10:03 +0530

    single string test case refactored

--- src/gaurav/stringcalculator/kata/StringCalculator.java
+++ src/gaurav/stringcalculator/kata/StringCalculator.java
@@ -8,8 +8,14 @@ public class StringCalculator {
     public int add(String input) {
         if(input.isEmpty())
             return 0;
+        else if(input.length()==1)
+            return convert2int(input);
 
         return -1;
+    }
+
+    private int convert2int(String inp){
+        return Integer.parseInt(inp);
     }
 
 }
